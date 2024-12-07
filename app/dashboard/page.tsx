f7ff8d494df66db9417d51ed5778aedb058bd432
baran <baran.kilicci@icloud.com>
2024-12-07 17:05:52 +0300

    authentication with kinde-auth

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+  return (
+    <div>
+      first commit
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import {
+  RegisterLink,
+  LoginLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
 
 export default function Home() {
   return (
     <div>
-      First Commit
+      <Button asChild>
+        <LoginLink>Login</LoginLink>
+      </Button>
+      <Button asChild>
+        <RegisterLink>Sign up</RegisterLink>
+      </Button>
     </div>
   );
 }
